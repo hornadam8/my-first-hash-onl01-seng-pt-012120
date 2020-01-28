a487784f3310c90f14382e6474649c189bf46656
Adam G Horn <hornadam8@gmail.com>
2020-01-28 00:23:41 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -6,9 +6,6 @@ def shipping_manifest
  the_manifest = {"whale bone corsets" => "5","porcelain vases" => "2","oil paintings" => "3"}
 end
 
-
-end
-
 def retrieval
   shipping_manifest = {
                   "whale bone corsets" => 5, 
